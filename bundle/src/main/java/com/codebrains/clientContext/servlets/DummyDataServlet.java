6ee30509725bf18efd58eb8a2ce43d7bbea7461c
Ankit Gubrani <ankit.gubrani@intelligrape.com>
2014-10-31 16:39:08 +0530

    Added response header in the servlet to make it work with JSONP store component

--- bundle/src/main/java/com/codebrains/clientContext/servlets/DummyDataServlet.java
+++ bundle/src/main/java/com/codebrains/clientContext/servlets/DummyDataServlet.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.Date;
 
 @SlingServlet(label = "Dummy Data Servlet",description = "This is a dummy servlet that returns Dummy data as JSON Response",
         generateComponent = false, paths = {"/bin/client/data"}, methods = {"GET", "POST"})
@@ -28,6 +29,14 @@ public class DummyDataServlet extends SlingAllMethodsServlet {
 
     protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) {
         try {
+            response.setContentType("application/json");
+            response.setHeader("Content-Type", "application/json");
+            response.addHeader("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate");
+            response.addHeader("Pragma", "no-cache");
+            response.addHeader("Last-Modified", new Date().toString());
+            response.addHeader("Expires", "Fri, 01 Jan 1990 00:00:00 GMT");
+            response.addHeader("Connection", "close");
+
             JSONObject jsonObject = new JSONObject();
 
             jsonObject.put("name", "Ankit Gubrani");
@@ -35,8 +44,16 @@ public class DummyDataServlet extends SlingAllMethodsServlet {
             jsonObject.put("city", "delhi");
             jsonObject.put("vertical", "cq5");
 
+            if(request.getParameter("callback") != null && !"".equalsIgnoreCase(request.getParameter("callback").toString())) {
+                response.getWriter().print(request.getParameter("callback")+"(");
+            }
+
             //Returnning the Dummay JSON Object.
-            response.getWriter().println(jsonObject.toString());
+            response.getWriter().print(jsonObject.toString());
+
+            if(request.getParameter("callback") != null && !"".equalsIgnoreCase(request.getParameter("callback").toString())) {
+                response.getWriter().print(")");
+            }
         } catch (JSONException e) {
             LOGGER.error("Json Exception occured in DummyDataServlet while adding data ", e);
         } catch (IOException e) {
